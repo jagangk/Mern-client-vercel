2226385d46a0a99faa6d8a4d00c9931b1da23e4a
jagangk <jaganvijayakumar2003@gmail.com>
2024-04-15 15:44:01 +0530

    updated file

---
+++ src/header.js
@@ -0,0 +1,82 @@
+import { useContext, useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { UserContext } from './userContext';
+
+export default function Header() {
+  const { setUserInfo, userInfo } = useContext(UserContext);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const url = `${process.env.REACT_APP_API_URL}/profile`;
+    const token = userInfo?.token;
+    fetch(url,{
+      credentials: 'include',
+      headers: {
+        'Authorization': `Bearer ${token}`,
+      },
+    })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`Failed to fetch profile: ${response.statusText}`);
+        }
+        return response.json();
+      })
+      .then(userInfo => {
+        setUserInfo(userInfo);
+      })
+      .catch(error => {
+        console.error('Error fetching profile:', error.message);
+      });
+  }, [setUserInfo]);
+
+  //logout
+  async function logout() {
+    const url = `${process.env.REACT_APP_API_URL}/logout`;
+    const response = await fetch(url, {
+      credentials: 'include',
+      method: 'POST',
+    });
+
+    setUserInfo(null);
+
+    if (response.ok) {
+      alert('Log out successful');
+      navigate('/');
+      setUserInfo(null);
+    }
+  }
+
+  const username = userInfo?.username;
+
+  return (
+    <header>
+      <div className="logo">
+          <img className="icon" src="b.png"/>
+          <Link className="web-title" to="/">Blogstera</Link>
+      </div>
+      <nav>
+        {username ? (
+          <>
+            <div className="dropdown">
+              <span>
+                Welcome<Link className="dropbtn"> {username}</Link>
+              </span>
+              <div className="dropdown-content">
+                <Link to="/create">Create post</Link>
+                <Link to="/">Feeds</Link>
+                <Link to="/contact">Contact</Link>
+                <Link onClick={logout}>Logout</Link>
+              </div>
+            </div>
+          </>
+        ) : (
+          <>
+            <Link to="/login">Login</Link>
+            <Link to="/register">Register</Link>
+            <Link to="/contact">Contact</Link>
+          </>
+        )}
+      </nav>
+    </header>
+  );
+}
---
+++ src/pages/postpage.js
@@ -0,0 +1,56 @@
+import { useContext, useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import { format } from 'date-fns';
+import { UserContext } from "../userContext";
+
+export default function PostPage() {
+    const [postInfo, setPostInfo] = useState(() => {
+        // Try to get postInfo from localStorage if available
+        const storedPostInfo = localStorage.getItem('postInfo');
+        return storedPostInfo ? JSON.parse(storedPostInfo) : null;
+    });
+    const { id } = useParams();
+    const { userInfo } = useContext(UserContext);
+
+    useEffect(() => {
+        const url = `${process.env.REACT_APP_API_URL}/post/${id}`;
+        fetch(url)
+            .then(response => {
+                response.json().then(postInfo => {
+                    setPostInfo(postInfo);
+                    // Store postInfo in localStorage
+                    localStorage.setItem('postInfo', JSON.stringify(postInfo));
+                });
+            });
+    }, [id]); // Include id in dependencies to trigger effect on id change
+
+    if (!postInfo) return '';
+
+    const url_photo = `${postInfo.cover}`;
+
+    return (
+        <div className='post-page'>
+            <h2>{postInfo.title}</h2>
+            <div className='post-info'>
+                <div className='author'>{postInfo.author.username}</div>
+                <time>{format(new Date(postInfo.createdAt), 'dd/LL/yyyy')}</time>
+                {userInfo.id === postInfo.author._id && (
+                    <div className='edit-btn'>
+                        <Link to={`/edit/${postInfo._id}`}>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
+                            </svg>
+                            Edit Post
+                        </Link>
+                    </div>
+                )}
+
+            </div>
+
+            <div className='image'>
+                <img src={url_photo} alt="Post Cover" />
+            </div>
+            <div dangerouslySetInnerHTML={{ __html: postInfo.content }} />
+        </div>
+    );
+}
