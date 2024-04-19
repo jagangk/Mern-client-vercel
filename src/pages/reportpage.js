d70ffa681ce0962ba1286553c6650786642c0960
Jagan GK <jaganvijayakumar2003@gmail.com>
2024-04-19 18:23:58 +0530

    Update reportpage.js

--- src/pages/reportpage.js
+++ src/pages/reportpage.js
@@ -101,15 +101,9 @@ export default function ReportPost() {
         <h3>Reporting Guidelines:</h3>
             <p>If you believe that content on our platform violates our community guidelines, please report it to us. We take reports seriously and will review them carefully.</p>
           <h3>Community Guidelines:</h3>
-            <p>*Create respectful content - no explicit or offensive material, support for illegal activities, or piracy links. Respect user privacy, engage in civil communication, and report violations for a positive community. Violations may lead to content removal or account actions.</p>
+            <p>Create respectful content - no explicit or offensive material, support for illegal activities, or piracy links. Respect user privacy, engage in civil communication, and report violations for a positive community. Violations may lead to content removal or account actions.</p>
           <h3>Action Upon Violation:</h3>
-            <p>When a report matches our guidelines and we find that a user has violated our community guidelines, we will take appropriate action.</p>
-          <ul>
-             <li>Content violating guidelines will be removed.</li>
-             <li>Repeat offenders may face account restrictions or bans.</li>
-             <li>We strive to maintain a safe and respectful community and appreciate your cooperation in upholding these standards.</li>
-          </ul>
-          <p>For further</p>
+            <p>When a report matches our guidelines and we find that a user has violated our community guidelines, we will take appropriate action. Content violating guidelines will be removed also repeat offenders may face account restrictions or bans.We strive to maintain a safe and respectful community and appreciate your cooperation in upholding these standards.</p>
         <Footer/>
       </div>
     </>
