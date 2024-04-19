cf79cf8e7e516215d08df15e5913fd9b10f2eca6
Jagan GK <jaganvijayakumar2003@gmail.com>
2024-04-19 22:09:37 +0530

    Update userContext.js

--- src/userContext.js
+++ src/userContext.js
@@ -1,11 +1,19 @@
-import {createContext, useState } from "react";
+import {createContext, useState, useEffect } from "react";
 export const UserContext = createContext({}); 
 
 export function UserContextProvider({children}) {
     const [userInfo,setUserInfo] = useState({});
+    const [posts, setPosts] = useState([]);
+    useEffect(() => {
+        const url = `${process.env.REACT_APP_API_URL}/post`;
+        fetch(url)
+            .then(response => response.json())
+            .then(data => setPosts(data))
+            .catch(error => console.error("Error fetching posts:", error));
+    }, []);
 
     return(
-        <UserContext.Provider value={{userInfo,setUserInfo}}>
+        <UserContext.Provider value={{userInfo,setUserInfo,posts,setPosts}}>
              {children}
         </UserContext.Provider> 
     );
