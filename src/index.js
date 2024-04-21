ec2b5a6c562a906887b19548ee565bbbc4529323
Jagan GK <jaganvijayakumar2003@gmail.com>
2024-04-21 21:36:48 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,13 +5,10 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import '../node_modules/font-awesome/css/font-awesome.min.css'; 
-import { createRoot } from 'react-dom/client';
-
-const root = document.getElementById('root');
-const rootElement = createRoot(root);
-rootElement.render(
+ReactDOM.render(
   <BrowserRouter>
     <App />
-  </BrowserRouter>
+  </BrowserRouter>,
+  document.getElementById('root')
 );
 reportWebVitals();
