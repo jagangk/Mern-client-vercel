0ce81cf8c34eb7be43d81c83372722d2357fe260
Jagan GK <jaganvijayakumar2003@gmail.com>
2024-04-28 15:47:25 +0530

    Update footer.js
    footer update

--- src/footer.js
+++ src/footer.js
@@ -2,7 +2,6 @@ import { Link} from 'react-router-dom';
 export default function Footer() {
     return (
         <footer>
-            <hr></hr>
             <h2>Sign up to our newsletter to receive updates</h2>
             <p>Don't miss out on the latest news, offers, and exciting developments. Subscribe now for a front-row seat to all things new and noteworthy!</p>
             <div class='newsletter'>
@@ -20,18 +19,12 @@ export default function Footer() {
 
             <div class='social-route' >
             <ul class="social-menu">
-              <li><a class='facebook'  href="#"><i class="fa fa-facebook"></i></a></li>
-              <li><a class='twitter'  href="#"><i class="fa fa-twitter"></i></a></li>
-              <li><a target='_blank' class='dribble'  href="https://github.com/jagangk"><i class="fa fa-github"></i></a></li>
-              <li><a target='_blank' class='linkedin'  href="https://www.linkedin.com/in/jagan-vijayakumar-13a6a1221"><i class="fa fa-linkedin"></i></a></li>   
+              <li><a class='facebook'  href="/"><i class="fa fa-facebook"></i></a></li>
+              <li><a class='twitter'  href="/"><i class="fa fa-twitter"></i></a></li>
+              <li><a class='dribble'  href="https://github.com/jagangk"><i class="fa fa-github"></i></a></li>
+              <li><a class='linkedin'  href="https://www.linkedin.com/in/jagan-vijayakumar-13a6a1221"><i class="fa fa-linkedin"></i></a></li>   
             </ul>
           </div>
     </footer>
     );
 }
-
-
-
-
-
-    
