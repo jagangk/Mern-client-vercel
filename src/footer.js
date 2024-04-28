445696e7d0a08b21c0138344547d477f1b0cafed
jagangk <jaganvijayakumar2003@gmail.com>
2024-04-28 17:59:01 +0530

    updated copyrght info in footer

--- src/footer.js
+++ src/footer.js
@@ -1,13 +1,13 @@
 import { Link} from 'react-router-dom';
 export default function Footer() {
     return (
-        <footer>
+        <><footer>
             <div className='footer-div'>
-            <h2>Sign up to our newsletter to receive updates!</h2>
-            <p>Don't miss out on the latest news, offers, and exciting developments. Subscribe now for a front-row seat to all things new and noteworthy!</p>
+                <h2>Sign up to our newsletter to receive updates!</h2>
+                <p>Don't miss out on the latest news, offers, and exciting developments. Subscribe now for a front-row seat to all things new and noteworthy!</p>
             </div>
             <div class='newsletter'>
-                <input type="email" class='f-input'  placeholder="Email"   />
+                <input type="email" class='f-input' placeholder="Email" />
                 <button class='f-btn'>Subscribe</button>
             </div>
             <div class='footer-route'>
@@ -19,14 +19,16 @@ export default function Footer() {
                 </ul>
             </div>
 
-            <div class='social-route' >
-            <ul class="social-menu">
-              <li><a class='facebook'  href="/"><i class="fa fa-facebook"></i></a></li>
-              <li><a class='twitter'  href="/"><i class="fa fa-twitter"></i></a></li>
-              <li><a class='dribble'  href="https://github.com/jagangk"><i class="fa fa-github"></i></a></li>
-              <li><a class='linkedin'  href="https://www.linkedin.com/in/jagan-vijayakumar-13a6a1221"><i class="fa fa-linkedin"></i></a></li>   
-            </ul>
-          </div>
-    </footer>
+            <div class='social-route'>
+                <ul class="social-menu">
+                    <li><a class='facebook' href="/"><i class="fa fa-facebook"></i></a></li>
+                    <li><a class='twitter' href="/"><i class="fa fa-twitter"></i></a></li>
+                    <li><a class='dribble' href="https://github.com/jagangk"><i class="fa fa-github"></i></a></li>
+                    <li><a class='linkedin' href="https://www.linkedin.com/in/jagan-vijayakumar-13a6a1221"><i class="fa fa-linkedin"></i></a></li>
+                </ul>
+            </div>
+            <p className='copyright-info' >© 2024 Blogstera. All rights reserved.</p>
+        </footer>
+    </>
     );
 }
