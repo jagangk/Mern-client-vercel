0d9d911ecb1bb790a9edc0b7493cc0787b368241
Jagan GK <jaganvijayakumar2003@gmail.com>
2024-05-12 20:26:39 +0530

    Update indexpage.js

--- src/pages/indexpage.js
+++ src/pages/indexpage.js
@@ -1,6 +1,7 @@
 import { useContext, useEffect } from "react";
 import { UserContext } from "../userContext"; 
 import Post from "../post";
+import { Link } from "react-router-dom";
 import Footer from "../footer";
 
 export default function IndexPage() {
@@ -19,6 +20,7 @@ export default function IndexPage() {
     return (
         <>
             <div className='home-index'>
+                <Link to="/"><img src="/blogsterabanner.png" alt="Blog Logo"/></Link>
                 <h1>Feeds</h1>
             </div>
             {posts.length > 0 ? (
