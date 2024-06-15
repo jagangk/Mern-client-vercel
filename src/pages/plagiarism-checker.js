6f080b6c5e6c28b55d9839b97327ae326dba65b5
jagangk <jaganvijayakumar2003@gmail.com>
2024-06-15 16:49:03 +0530

    fixed skeleton errors on plag ai

--- src/pages/plagiarism-checker.js
+++ src/pages/plagiarism-checker.js
@@ -1,6 +1,5 @@
+import React, { useState, useEffect } from 'react';
 import { Helmet } from "react-helmet";
-import { useEffect } from "react";
-import React, { useState } from 'react';
 import { Alert, AlertIcon, AlertTitle, useDisclosure } from "@chakra-ui/react";
 import Skeleton from '@mui/material/Skeleton';
 import Box from '@mui/material/Box';
@@ -17,23 +16,23 @@ export default function PlagiarismChecker() {
     let timer;
 
     if (isSuccessOpen) {
-        timer = setTimeout(() => {
-            onCloseSuccess();
-            setSuccessMessage('');
-        },3000);
+      timer = setTimeout(() => {
+        onCloseSuccess();
+        setSuccessMessage('');
+      }, 3000);
     }
 
-      return () => {
-        clearTimeout(timer);
-      };
+    return () => {
+      clearTimeout(timer);
+    };
 
-}, [isSuccessOpen]);
-  
-useEffect(() => {
-  if (rewrittenText) {
-    setIsLoaded(true);
-  }
-}, [rewrittenText]);
+  }, [isSuccessOpen]);
+
+  useEffect(() => {
+    if (rewrittenText) {
+      setIsLoaded(true);
+    }
+  }, [rewrittenText]);
 
   function handleTextChange(event) {
     setText(event.target.value);
@@ -42,6 +41,8 @@ useEffect(() => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     setIsLoading(true);
+    setIsLoaded(false);
+    setRewrittenText('');
 
     try {
       const url = `${process.env.REACT_APP_API_URL}/api/plagiarism-check`;
@@ -73,33 +74,32 @@ useEffect(() => {
         <meta name="description" content="PlagGuard AI is an advanced Gemini 1.5 powered tool designed to help you create plagiarism-free content. PlagGuard not only detects potential plagiarism in your text but also provides intelligent suggestions and edits to ensure your work is original and authentic." />
       </Helmet>
       <form className="plag-remover">
-      {isSuccessOpen && (
-        <Alert
-          status='success'
-          variant='subtle'
-          display='flex'
-          flexDirection='row'
-          alignItems='center'
-          justifyContent='center'
-          gap='10px'
-          textAlign='center'
-          height='80px'
-          colorScheme="red"
-          bg='#2E8B57'
-          borderRadius='10px'
-          fontSize='small'
-          margin='20px'
-          maxWidth= '70%'
-        >
-          <AlertIcon boxSize='30px' mr={0} />
-          <AlertTitle mt={4} mb={1} fontSize='lg'>
-            {successMessage}
-          </AlertTitle>
-        </Alert>
-      )}
+        {isSuccessOpen && (
+          <Alert
+            status='success'
+            variant='subtle'
+            display='flex'
+            flexDirection='row'
+            alignItems='center'
+            justifyContent='center'
+            gap='10px'
+            textAlign='center'
+            height='80px'
+            colorScheme="red"
+            bg='#2E8B57'
+            borderRadius='10px'
+            fontSize='small'
+            margin='20px'
+            maxWidth='70%'
+          >
+            <AlertIcon boxSize='30px' mr={0} />
+            <AlertTitle mt={4} mb={1} fontSize='lg'>
+              {successMessage}
+            </AlertTitle>
+          </Alert>
+        )}
 
         <div className="gemini-icon-container">
-          
           <div className="gicon-title">
             <img alt="ai logo" className="blogAI-icon" src="blog-ai.png" />
             <h1>PlagGuard AI</h1>
@@ -111,7 +111,7 @@ useEffect(() => {
           </div>
         </div>
 
-        <p>PlagGuard is an advanced Gemini 1.5 powered tool designed to help you create plagiarism free content. PlagGuard not only detects potential plagiarism in your text but also provides intelligent suggestions and edits to ensure your work is authentic.</p>
+        <p>PlagGuard is an advanced Gemini 1.5 powered tool designed to help you create plagiarism-free content. PlagGuard not only detects potential plagiarism in your text but also provides intelligent suggestions and edits to ensure your work is authentic.</p>
 
         <div className="search-container">
           <input value={text} onChange={handleTextChange} type="text" placeholder="Enter a text" className="search-input" />
@@ -127,15 +127,14 @@ useEffect(() => {
         {isLoading && (
           <div className="plagGuard-results" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '150px' }}>
             <Box width='90%'>
-              <Skeleton  variant="rectangular" width="100%" height={20} style={{ marginBottom: 8 }} />
-              <Skeleton  variant="rectangular" width="100%" height={20} style={{ marginBottom: 8 }} />
-              <Skeleton  variant="rectangular" width="100%" height={20} />
+              <Skeleton variant="rectangular" width="100%" height={20} style={{ marginBottom: 8 }} />
+              <Skeleton variant="rectangular" width="100%" height={20} style={{ marginBottom: 8 }} />
+              <Skeleton variant="rectangular" width="100%" height={20} />
             </Box>
           </div>
         )}
 
-
-        {isLoaded && rewrittenText && (
+        {!isLoading && isLoaded && rewrittenText && (
           <div className="plagGuard-results">
             <div className='plag-toolbar'>
               <img alt="ai" className="toolbar-icon" src="blog-ai.png" />
@@ -144,7 +143,7 @@ useEffect(() => {
                 navigator.clipboard.writeText(rewrittenText)
                   .then(() => {
                     setSuccessMessage("Copied to clipboard");
-                    onOpenSuccess(); 
+                    onOpenSuccess();
                   })
                   .catch(err => {
                     console.error('Failed to copy text:', err);
@@ -154,7 +153,6 @@ useEffect(() => {
             <p>{rewrittenText}</p>
           </div>
         )}
-
       </form>
     </>
   );
